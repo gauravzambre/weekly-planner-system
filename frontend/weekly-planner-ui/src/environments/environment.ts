45f7a67d25928c0b82d7a4cee6a50c856449f36d
gauravzambre <91050644+gauravzambre@users.noreply.github.com>
2026-03-15 19:03:26 +0530

    Add environment.ts  for Angular configuration

--- frontend/weekly-planner-ui/src/environments/environment.ts
+++ frontend/weekly-planner-ui/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://weekly-planner-cloud-platform-api-brhmg5cmexbbavhc.centralindia-01.azurewebsites.net'
+  apiBaseUrl: 'https://zenova-api-hwcfbbejhae9dsex.centralindia-01.azurewebsites.net'
 };
